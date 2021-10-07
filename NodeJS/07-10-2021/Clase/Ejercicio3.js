195cf4fdd0878d87cd8932a17204aac3266c7e6e
Seevii16 <sevillanito435@gmail.com>
2021-10-07 21:01:17 +0200

    Modificacion ejercicio 3

--- NodeJS/07-10-2021/Clase/Ejercicio3.js
+++ NodeJS/07-10-2021/Clase/Ejercicio3.js
@@ -1,7 +1,6 @@
 const url = new URL('https://developer.mozilla.org:80/en-US/docs/Web/API/URL/search?q=123');
 
 console.log(`Protocolo:${url.protocol}`); 
-console.log(`Hash:${url.hash}`);
 console.log(`Host:${url.host}`); 
 console.log(`Hostname:${url.hostname}`);
 console.log(`Href:${url.href}`); 
@@ -9,4 +8,4 @@ console.log(`Origin:${url.origin}`);
 console.log(`Pathname:${url.pathname}`);
 console.log(`Puerto:${url.port}`); 
 console.log(`Search:${url.search}`);
-console.log(`Username:${url.username}`);
+
