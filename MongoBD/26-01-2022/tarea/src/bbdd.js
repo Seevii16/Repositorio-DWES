3e0946624fd3fece9a140a93e5ff83097fd18b50
Seevii16 <sevillanito435@gmail.com>
2022-01-31 18:36:50 +0100

    Update bbdd.js

--- MongoBD/26-01-2022/tarea/src/bbdd.js
+++ MongoBD/26-01-2022/tarea/src/bbdd.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/baseDatos')
+mongoose.connect('mongodb://localhost/tarea')
     .then(db => console.log(db.connection.host))
     .catch(err => console.error(err));
